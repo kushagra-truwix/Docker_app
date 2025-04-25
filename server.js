2530e5699accff0ab62fee4b6c5122e86f75f93c
kushagra tiwari <kushagrat@truwix.com>
2025-04-25 18:47:43 +0530

    dfd

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors'); 
 
 app.use(cors());
-const PORT = process.env.PORT || 8000;
+const PORT = 8000;
 
 app.get('/', (req, res) => {
   res.send('Hello, Dockerized Node.js!');
